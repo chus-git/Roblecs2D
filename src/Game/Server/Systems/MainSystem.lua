aff0630835e9d7a9e629035bb30938ff2aaf079f
USERSAD\jloregal <jesus.lorenzogallego@emeal.nttdata.com>
2025-08-12 13:28:16 +0200

    minimal changes

--- src/Game/Server/Systems/MainSystem.lua
+++ src/Game/Server/Systems/MainSystem.lua
@@ -2,7 +2,6 @@ local MainSystem = require(game.ReplicatedStorage.Source.System).extend()
 
 function MainSystem:init()
 	self.playerSystem = self:create(game.ReplicatedStorage.Modules.Essentials.Systems.PlayerSystem)
-    self.playerSystem:init()
     self.playerSystem:load()
 end
 
